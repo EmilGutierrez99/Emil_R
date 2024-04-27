a8af90343aaac0ccb9f3b98f256ff9c798aef8d4
EmilGutierrez99 <emilgutierrez81@gmail.com>
2024-04-26 21:47:24 -0400

    prueba de Correción scrip.sh

--- scrip.sh
+++ scrip.sh
@@ -4,12 +4,12 @@
 filename="ejemplo.csv"
 
 # Escribir la cabecera del CSV
-echo "Nombre, Edad, Correo" > "$filename"
+echo "Nombre, Edad, Celular,  Correo" > "$filename"
 
 # Bucle para generar 60 filas de datos en el CSV
 for ((i=1; i<=60; i++))
 do
-    echo "Nombre$i, $i, Correo$i" >> "$filename"
+    echo "Nombre$i, $i, $i$i$i$i, Correo$i@gmail.com" >> "$filename"
 done
 
 echo "¡Archivo CSV '$filename' con 60 filas creado exitosamente!"
