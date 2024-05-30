4d7e831b5cbee6b7d2f9a7875bd265c957a0efaf
EmilGutierrez99 <emilgutierrez81@gmail.com>
2024-05-30 11:41:27 -0400

    PRUEBA

--- AAScrip.sh
+++ AAScrip.sh
@@ -2,28 +2,30 @@
 #ubuntu 20.04
 
 #definir los repositorios y todas las constantes
+$DIRECTORIO_HTML="/var/www/html"
 
-echo -e "\n\nUpdating Apt Packages and upgrading latest patches\n"
-sudo apt-get update -y && sudo apt-get upgrade -y
 
-echo -e "\n\nInstalling Apache2 Web server\n"
-sudo apt update && sudo apt install apache2 -y
+#echo -e "\n\nUpdating Apt Packages and upgrading latest patches\n"
+#sudo apt-get update -y && sudo apt-get upgrade -y
 
-echo -e "\n\nInstalling PHP & Requirements\n"
-sudo apt install -y php libapache2-mod-php -y
-sudo apt install php-curl php-gd php-json php-mbstring php-xml -y
+#echo -e "\n\nInstalling Apache2 Web server\n"
+#sudo apt update && sudo apt install apache2 -y
 
-echo -e "\n\nInstalling MySQL\n"
-sudo apt install mysql-server php-mysql -y
+#echo -e "\n\nInstalling PHP & Requirements\n"
+#sudo apt install -y php libapache2-mod-php -y
+#sudo apt install php-curl php-gd php-json php-mbstring php-xml -y
 
-echo -e "\n\nInstalling net-tools\n"
-sudo apt install net-tools -y
+#echo -e "\n\nInstalling MySQL\n"
+#sudo apt install mysql-server php-mysql -y
+
+#echo -e "\n\nInstalling net-tools\n"
+#sudo apt install net-tools -y
 #seteando el espacio de trabajo
 cd /var/ #cd /var/
 
 #crear la carpeta www/html  verificar si existe primero sino crearla
 echo -e "\n\nVerificacion de Directorios\n"
- if [ -d "/var/www/html" ] 
+ if [ -d "$DIRECTORIO_HTML" ] 
 then
     echo "El directorio /var/www/html ya existe."
 else
@@ -35,45 +37,45 @@ cd /var/www/html/
 sudo rm -r *
 
 #crear un www temporal
-echo -e "\n\nCreando directorio temporal\n"
-cd /var/www/
-sudo mkdir temp
+#echo -e "\n\nCreando directorio temporal\n"
+#cd /var/www/
+#sudo mkdir temp
 
 #clonar el repositorio de github en la carpeta temporal 
-echo -e "\n\nClonamos repositorio de github\n"
-cd /var/www/temp/
-sudo git clone https://github.com/EmilGutierrez99/Emil_R.git
+#echo -e "\n\nClonamos repositorio de github\n"
+#cd /var/www/temp/
+#sudo git clone https://github.com/EmilGutierrez99/Emil_R.git
 
 #hacer los checkouts de las ramas y actualizar ramas
-echo -e "\n\nCambiamos de rama a la rama de desarrollo\n"
-cd /var/www/temp/Emil_R/
-sudo git checkout Actualizaciones
-sudo git pull
+#echo -e "\n\nCambiamos de rama a la rama de desarrollo\n"
+#cd /var/www/temp/Emil_R/
+#sudo git checkout Actualizaciones
+#sudo git pull
 
-cd /var/www/temp/
+#cd /var/www/temp/
 
 #copiamos el contenido de la carpeta temporal a la carpeta html
-echo -e "\n\nCopiamos el contenido de la carpeta temporal a la carpeta html\n"
-sudo cp -r  Emil_R/ /var/www/html/
+#echo -e "\n\nCopiamos el contenido de la carpeta temporal a la carpeta html\n"
+#sudo cp -r  Emil_R/ /var/www/html/
 
 
 #una vez copiado todo, eliminamos la carpeta temporal
-echo -e "\n\nEliminamos la carpeta temporal\n"
-cd /var/www/
-sudo rm -r temp
+#echo -e "\n\nEliminamos la carpeta temporal\n"
+#cd /var/www/
+#sudo rm -r temp
 
-echo -e "\n\nMovemos el contenido de la carpeta Emil_R a la carpeta html\n"
-cd /var/www/html/Emil_R/
-sudo mv CodigoFFin.php /var/www/html/index.php
-sudo mv img /var/www/html/
-sudo mv Datos_w.csv /var/www/html/
-sudo mv new-email.html /var/www/html/
+#echo -e "\n\nMovemos el contenido de la carpeta Emil_R a la carpeta html\n"
+#cd /var/www/html/Emil_R/
+#sudo mv CodigoFFin.php /var/www/html/index.php
+#sudo mv img /var/www/html/
+#sudo mv Datos_w.csv /var/www/html/
+#sudo mv new-email.html /var/www/html/
 
-echo -e "\n\nEliminamos la carpeta Emil_R\n"
-cd /var/www/html/
-sudo rm -r Emil_R
+#echo -e "\n\nEliminamos la carpeta Emil_R\n"
+#cd /var/www/html/
+#sudo rm -r Emil_R
 
-echo -e "\n\nFin\n"
+#echo -e "\n\nFin\n"
 
 
 
