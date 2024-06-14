29fd39b9925859a29c811425d631a92125509f7f
EmilGutierrez99 <emilgutierrez81@gmail.com>
2024-06-14 18:54:24 -0400

    apache correccion

--- AAA.sh
+++ AAA.sh
@@ -8,13 +8,7 @@ echo "Instalando Apache..."
 # Instalar Apache
 sudo apt install -y apache2
 
-# Habilitar el servicio Apache para que arranque automáticamente al iniciar el sistema
-sudo systemctl enable apache2
-
-# Iniciar el servicio Apache
-sudo systemctl start apache2
-
-# Mostrar el estado del servicio Apache
-sudo systemctl status apache2
+#verificamos version
+sudo apache2 -v
 
 echo "Apache se ha instalado y configurado correctamente de forma desatendida."
