535505f89e8106fa0373e545f2305667e3ef23d6
EmilGutierrez99 <emilgutierrez81@gmail.com>
2024-06-16 16:14:47 -0400

    3.2

--- MYSQL_Scrip.sh
+++ MYSQL_Scrip.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
-REPOSITORIO_GITHUB="https://github.com/EmilGutierrez99/Emil_R.git"
+REPOSITORIO_GITHUB1="https://github.com/EmilGutierrez99/Emil_R.git"
+DIRECTORIO_TEMPORAL_GITHUB1="/home/user/temporal/Emil_R"
+DIR_TEMPORAL="/home/user/temporal"
+DIR_USER="/home/user"
 
-echo "Movemos el contenido de la carpeta Emil_R a la carpeta html"
+#crear un www temporal
+echo -e "\n\nCreando directorio temporal\n"
+sudo mkdir $DIR_TEMPORAL
 
-sudo mv CodigoFFin.php $DIRECTORIO_HTML/index.php
-sudo mv img $DIRECTORIO_HTML/
-sudo mv Datos_w.csv $DIRECTORIO_HTML/
-sudo mv new-email.html $DIRECTORIO_HTML/
+#clonar el repositorio de github en la carpeta temporal 
+echo -e "\n\nClonamos repositorio de github\n"
+cd $DIR_TEMPORAL/
+sudo git clone $REPOSITORIO_GITHUB1
+
+#hacer los checkouts de las ramas y actualizar ramas
+echo -e "\n\nCambiamos de rama a la rama de desarrollo\n"
+cd $DIRECTORIO_TEMPORAL_GITHUB1/
+sudo git checkout Actualizaciones
+sudo git pull
+
+echo -e "\n\nMovemos el contenido de la carpeta Emil_R a la carpeta html\n"
+
+sudo mv APACHE_Scrip.sh $DIR_USER/
+sudo mv PHP_Scrip.sh $DIR_USER/
+
+
+#una vez copiado todo, eliminamos la carpeta temporal
+echo -e "\n\nEliminamos la carpeta temporal\n"
+cd $DIR_USER/
+sudo rm -r temporal
+
+echo -e "\n\nFin\n"
+
 # Actualizar la lista de paquetes
 sudo apt update -y
 
@@ -26,8 +51,11 @@ echo "MySQL se ha instalado y configurado correctamente de forma desatendida."
 
 
 echo "Instalando Apache..."
-directorio_apache="/home/user/"
 
-cd $directorio_apache
+cd $DIR_USER/
 chmod +x APACHE_Scrip.sh
 ./APACHE_Scrip.sh
+
+cd $DIR_USER/
+chmod +x PHP_Scrip.sh
+./PHP_Scrip.sh
